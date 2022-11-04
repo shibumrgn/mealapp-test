31823ba8d4a4963aeb266adabc3ce2886960a52d
Shibu Murugan <shibumrgn@gmail.com>
2022-11-04 19:56:21 +0530

    clearing lint issues

--- src/screens/RecipeDetails.tsx
+++ src/screens/RecipeDetails.tsx
@@ -1,15 +1,13 @@
-import { ScrollView, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import Toolbar from '../components/Toolbar'
+import {ScrollView} from 'react-native';
+import React from 'react';
+import Toolbar from '../components/Toolbar';
 
 const RecipeDetails = () => {
-  return (
-    <ScrollView>
-        <Toolbar/>
-    </ScrollView>
-  )
-}
+    return (
+        <ScrollView>
+            <Toolbar/>
+        </ScrollView>
+    );
+};
 
-export default RecipeDetails
-
-const styles = StyleSheet.create({})
+export default RecipeDetails;
